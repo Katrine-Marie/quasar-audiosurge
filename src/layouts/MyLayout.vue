d93760a3d138e5db989efd9a57d69abb8695ac17
Katrine-Marie Burmeister <fjordscene@gmail.com>
2020-01-20 18:35:37 +0100

    Added dev files

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,49 @@
+
+<template>
+<q-layout view="lHh Lpr lFf">
+  <q-header elevated>
+    <q-toolbar>
+      <q-toolbar-title>
+        Quasar Audiosurge
+      </q-toolbar-title>
+
+      <q-btn color="white" text-color="primary">
+        Load File
+        <input
+          type="file"
+          class="q-uploader__input overflow-hidden absolute-full"
+          v-on:change="fileChosen"
+          ref="fileInput"
+          accept="audio/mpeg"
+        />
+      </q-btn>
+    </q-toolbar>
+  </q-header>
+
+  <q-page-container>
+    <router-view />
+  </q-page-container>
+</q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+import { EventBus } from '../services/event-bus.js'
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL,
+    fileChosen (file) {
+      EventBus.$emit('fileChosen', file)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,118 @@
+<template>
+  <q-page class>
+    <div class="audio-container">
+      <div class="row q-ma-md">
+        <div class="col-12">
+          <div id="waveform"></div>
+        </div>
+      </div>
+    </div>
+    <div class="controls row flex flex-center fixed-bottom q-pb-md q-pt-md shadow-10">
+      <div class>
+        <q-btn
+          color="primary"
+          flat
+          round
+          icon="fast_rewind"
+          size="xl"
+          @click="wavesurfer.skipBackward(1)"
+        />
+        <q-circular-progress v-if="isLoading" size="72px" indeterminate color="primary" />
+        <q-btn
+          v-if="isPlaying"
+          color="primary"
+          round
+          icon="pause"
+          size="xl"
+          @click="wavesurfer.playPause()"
+        />
+        <q-btn
+          v-if="!isPlaying && !isLoading"
+          color="primary"
+          round
+          icon="play_arrow"
+          size="xl"
+          @click="wavesurfer.playPause()"
+        />
+        <q-btn
+          color="primary"
+          flat
+          round
+          icon="fast_forward"
+          size="xl"
+          @click="wavesurfer.skipForward(1)"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import WaveSurfer from 'wavesurfer.js'
+import { EventBus } from '../services/event-bus.js'
+export default {
+  data: () => ({
+    wavesurfer: null,
+    isLoading: false
+  }),
+  async mounted () {
+    EventBus.$on('fileChosen', file => {
+      this.loadFile(file)
+    })
+    if (!this.wavesurfer) this.createWaveSurfer()
+  },
+  computed: {
+    isPlaying () {
+      if (!this.wavesurfer) return false
+      return this.wavesurfer.isPlaying()
+    }
+  },
+  methods: {
+    createWaveSurfer () {
+      this.wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        scrollParent: true,
+        hideScrollbar: true,
+        autocenter: true,
+        waveColor: '#fff',
+        progressColor: 'orchid',
+        cursorColor: '#fff',
+        barWidth: 3,
+        minPxPerSec: 50,
+        pixelRatio: 1
+      })
+      this.wavesurfer.on('error', err => {
+        console.error(err)
+        this.isLoading = false
+        this.$q.notify({ message: err })
+      })
+      this.wavesurfer.on('loading', () => {
+        this.isLoading = true
+      })
+      this.wavesurfer.on('ready', () => {
+        this.isLoading = false
+      })
+    },
+    loadFile (file) {
+      if (file.target.files.length === 0) return
+      this.wavesurfer.loadBlob(file.target.files[0])
+    }
+  },
+  name: 'PageIndex'
+}
+</script>
+<style>
+.controls {
+  background-color: white;
+}
+.audio-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url('../assets/audio.jpg') no-repeat center;
+  background-size: cover;
+}
+</style>
